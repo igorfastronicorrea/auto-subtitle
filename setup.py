83a8326b1d84f71add54de2320e137534c4fe565
Igor Fastroni <igor.fastroni@gmail.com>
2023-10-12 21:36:08 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     version="1.0",
     name="auto_subtitle_igor",
     packages=find_packages(),
-    py_modules=["auto_subtitle"],
+    py_modules=["auto_subtitle_igor"],
     author="Miguel Piedrafita",
     install_requires=[
         'openai-whisper',
     ],
     description="Automatically generate and embed subtitles into your videos",
     entry_points={
-        'console_scripts': ['auto_subtitle=auto_subtitle.cli:main'],
+        'console_scripts': ['auto_subtitle_igor=auto_subtitle_igor.cli:main'],
     },
     include_package_data=True,
 )
